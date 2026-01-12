dcdf985b9247e10f66be710db44323aa86d75c88
UNBunny <nikitakuharchuk07@gmail.com>
2026-01-12 21:13:10 +0600

    fix: динамический redirect_uri для OAuth на Render

--- public/config/oauth.php
+++ public/config/oauth.php
@@ -10,9 +10,10 @@
 
 return [
     'yandex' => [
-        'client_id' => $_ENV['YANDEX_CLIENT_ID'] ?? '',
-        'client_secret' => $_ENV['YANDEX_CLIENT_SECRET'] ?? '',
-        'redirect_uri' => 'http://localhost/admin/index.php',
-        'enabled' => !empty($_ENV['YANDEX_CLIENT_ID'])
+        'client_id' => $_ENV['OAUTH_CLIENT_ID'] ?? $_ENV['YANDEX_CLIENT_ID'] ?? '',
+        'client_secret' => $_ENV['OAUTH_CLIENT_SECRET'] ?? $_ENV['YANDEX_CLIENT_SECRET'] ?? '',
+        'redirect_uri' => $_ENV['OAUTH_REDIRECT_URI'] ?? 
+                         'https://' . ($_SERVER['HTTP_HOST'] ?? 'localhost') . '/admin/index.php',
+        'enabled' => !empty($_ENV['OAUTH_CLIENT_ID']) || !empty($_ENV['YANDEX_CLIENT_ID'])
     ]
 ];
